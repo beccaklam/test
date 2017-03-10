f67a74206f95717cb3e727be2ce6eaeb87d119ca
Rebecca Lam <rebeccal@mozillafoundation.org>
2017-03-10 14:54:09 -0500

    test

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+			$('#fullPage').fullpage({
+				anchors: ['firstPage', 'secondPage', '3rdPage'],
+				sectionsColor: ['#552582', '#FDB927', '#552582'],
+				slidesNavigation: true,
+			});
+		});
